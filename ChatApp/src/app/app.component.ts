48140b2346bff78700d7fd74565423eafb8a656d
dvir ben shlush <dvirbsh95@gmail.com>
2022-05-05 09:56:16 +0300

    added basic auth

--- ChatApp/src/app/app.component.ts
+++ ChatApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ChatApp';
+  constructor(private authService:AuthService){}
+
+  public signInWithGoogle(){
+    this.authService.signWithGoogle();
+  }
 }
--- ChatApp/src/environments/environment.ts
+++ ChatApp/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyD1_PmpSnAJAVbs_W20cdaOOFqEVpMUmbY",
+    authDomain: "chatapp-52f4a.firebaseapp.com",
+    projectId: "chatapp-52f4a",
+    storageBucket: "chatapp-52f4a.appspot.com",
+    messagingSenderId: "465908862416",
+    appId: "1:465908862416:web:c5d0815d62f8349a5e7dd5",
+    measurementId: "G-XKGVSJ5FJ4"
+  } 
 };
 
 /*
